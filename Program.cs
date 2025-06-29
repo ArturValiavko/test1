ee0448dad4a10c730008f2c00f59845cca37fd62
ArturValiavko <Artas840@gmail.com>
2025-06-29 19:03:36 +0300

    Update message in feature/hello-enhancement branch

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, git brach!");
+
+Console.WriteLine("HW");
+Console.WriteLine("Hw1");
+Console.WriteLine("Hw1");
+Console.WriteLine("Hw1");
+Console.WriteLine("Hw1"); 
+Console.WriteLine("Hw1"); 
