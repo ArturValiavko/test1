036d25dbdbd669cde20b611d3bfa1792e22b170b
ArturValiavko <Artas840@gmail.com>
2025-06-30 23:04:12 +0300

    Pridėtas int X,Y

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, git brach!");
+Console.WriteLine("Hello, git branch!");
 
 Console.WriteLine("HW");
 Console.WriteLine("Hw1");
 Console.WriteLine("Hw1");
 Console.WriteLine("Hw1");
-Console.WriteLine("Hw1"); 
-Console.WriteLine("Hw1"); 
+Console.WriteLine("Hw1");
+Console.WriteLine("Hw1");
+
+Console.WriteLine("test branch");
+int X = 1;
+int Y = 2;
