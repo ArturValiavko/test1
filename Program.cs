cbede67bdf2077ae6089ea5f66141e8f0b8c6e18
ArturValiavko <Artas840@gmail.com>
2025-07-03 00:31:20 +0300

    Task 3 if/if else/ else

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("C#"); //task1 
 Console.WriteLine("Mano vardas Artur");
-<<<<<<< HEAD
 
 Console.WriteLine("Sveikas C#");
-=======
->>>>>>> feature/task2
 
 Console.Write("O koks tavo vardas: ");
 string vardas = Console.ReadLine();
 Console.WriteLine($"Sveikas,{vardas}");
 
+//Task3 
+Console.WriteLine("Įvesk savo amžių;");
+string? input = Console.ReadLine();
+
+
+if (int.TryParse(input, out int age))
+{
+    Console.WriteLine($"Jūsų amžius: {age}");
+    if (age < 18)
+        Console.WriteLine("Nepilnametis");
+    else if (age < 65)
+        Console.WriteLine("Dar reikia eit į darbą");
+    else
+        Console.WriteLine("Nepasiseke");
+}
+else {
+    Console.WriteLine("Neteisingas formatas");
+}
