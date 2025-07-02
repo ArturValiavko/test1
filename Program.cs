7b0343aa87a7013c332bf35786d43b3210519d24
ArturValiavko <Artas840@gmail.com>
2025-07-02 23:52:33 +0300

    task2 - Consloe ReadLine

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("C#"); //task1 
-Console.WriteLine("Mano vardas Artur"); 
+Console.WriteLine("Mano vardas Artur");
 
-
-
+Console.Write("O koks tavo vardas: ");
+string vardas = Console.ReadLine();
+Console.WriteLine($"Sveikas,{vardas}");
 
